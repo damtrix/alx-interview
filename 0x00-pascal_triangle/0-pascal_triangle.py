48b419cc7f38f59d7a757c303e93e857226f7158
damtrix <oludamola.onarinde@gmail.com>
2023-06-28 17:29:58 +0100

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Pascal Triangle Interview Challenge"""
 
+
 def factorial(n):
     """returns the factorial of n"""
     if (n == 0):
@@ -8,20 +9,17 @@ def factorial(n):
     else:
         return n * factorial(n - 1)
 
+
 def pascal_triangle(n):
     """returns a list of lists of numbers
     representing the pascal triangle"""
     if n <= 0:
         return []
-    
     triangle = [0] * n
     for i in range(n):
-        #define
         new_array = []
         for j in range(i+1):
             c = factorial(i) // (factorial(j) * factorial(i - j))
             new_array.append('{}'.format(c))
-            
         triangle[i] = new_array
-            
     return triangle
