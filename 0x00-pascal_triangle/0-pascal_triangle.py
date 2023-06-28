cb44e4c35fc1cd8c7ab1eb1dabfba1bbcd8bd27a
damtrix <oludamola.onarinde@gmail.com>
2023-06-28 17:17:55 +0100

    again

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -16,7 +16,7 @@ def pascal_triangle(n):
     
     triangle = [0] * n
     for i in range(n):
-        
+        #define
         new_array = []
         for j in range(i+1):
             c = factorial(i) // (factorial(j) * factorial(i - j))
